bfabab94beebede4b830153e4901f2caaf47674e
Max <maximiliano.mino.s@mail.pucv.cl>
2025-05-09 12:34:55 -0400

    Squashed commit of the following:
    commit 67cbf220afe57c61195b1043e30d3edbef604441
    Author: TomasperezPucv <127423299+TomasperezPucv@users.noreply.github.com>
    Date:   Fri May 9 07:25:42 2025 -0400
    
        creacion de la pagina de notificaciones (html,ts,css)
    
    commit d89efe33be1d48dabefad0c570ff11b24c73a16e
    Author: TomasperezPucv <127423299+TomasperezPucv@users.noreply.github.com>
    Date:   Wed May 7 18:35:00 2025 -0400
    
        arrglo en el listado de eventos

--- src/app/pages/calendar/calendar.page.ts
+++ src/app/pages/calendar/calendar.page.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   standalone:false,
 })
 export class CalendarPage implements OnInit {
-
+  eventos: any[] = [
+    { id: 1, title: 'Evento 1 Observación:', start: '2023-10-01', end: '2023-10-02' },
+    { id: 2, title: 'Evento 2 Planteamiento:', start: '2023-10-05', end: '2023-10-06' },
+    { id: 3, title: 'Evento 3 lol xd:', start: '2023-10-10', end: '2023-10-12' },
+    { id: 4, title: 'Evento 4 Experimentacion:', start: '2023-10-15', end: '2023-10-16' },]
   constructor() { }
 
   ngOnInit() {
--- src/app/pages/notifications/notifications.page.ts
+++ src/app/pages/notifications/notifications.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-notifications',
@@ -7,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   standalone:false,
 })
 export class NotificationsPage implements OnInit {
-
+  
   constructor() { }
+  
 
   ngOnInit() {
   }
+  PlaceholderNotifications = [
+    {
+      date: 'Mar., 29 de abril',
+      notifications: Array(13).fill({ title: 'List item', icon: 'person-circle-outline' })
+    },
+    {
+      date: 'Mié., 30 de abril',
+      notifications: Array(12).fill({ title: 'List item', icon: 'person-circle-outline' })
+    },
+    {
+      date: 'Jue., 1 de mayo',
+      notifications: Array(8).fill({ title: 'List item', icon: 'person-circle-outline' })
+    }
+  ];
+ 
+
 
 }
